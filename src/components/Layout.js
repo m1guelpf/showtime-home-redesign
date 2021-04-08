6f41e012d67f190c5d9c629c13eecc0f135e8d34
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-08 19:17:02 +0200

    wip

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,10 @@
 import useStickyState from '@/hooks/sticky'
 import { MoonIcon, SunIcon } from '@heroicons/react/outline'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
 import ScrollToTop from './ScrollToTop'
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
 	const [containerClasses, setContainerClasses] = useState('theme-transition')
@@ -24,17 +25,31 @@ const Layout = ({ children }) => {
 	}
 
 	return (
-		<div className={`${containerClasses} ${isDarkMode ? 'dark' : ''}`}>
-			<div className="flex flex-col min-h-screen bg-gray-50 dark:bg-black relative">
-				<Header />
-				<ScrollToTop />
-				<main>{children}</main>
-				<button onClick={changeTheme} class="fixed bottom-3 right-3 bg-white dark:bg-black border-2 border-gray-800 dark:border-gray-400 p-1 rounded-full">
-					<ThemeIcon className="w-6 h-6 text-gray-800 dark:text-gray-400" />
-				</button>
-				<Footer />
+		<>
+			<Head>
+				<title>Showtime | Crypto Art</title>
+				<meta name="description" content="Discover and showcase crypto art" />
+				<meta property="og:type" content="website" />
+				<meta name="og:description" content="Discover and showcase crypto art" />
+				<meta property="og:image" content="https://showtime.m1guelpf.me/img/og.jpg" />
+				<meta name="og:title" content="Showtime" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content="Showtime" />
+				<meta name="twitter:description" content="Discover and showcase crypto art" />
+				<meta name="twitter:image" content="https://showtime.m1guelpf.me/img/og.jpg" />
+			</Head>
+			<div className={`${containerClasses} ${isDarkMode ? 'dark' : ''}`}>
+				<div className="flex flex-col min-h-screen bg-gray-50 dark:bg-black relative">
+					<Header />
+					<ScrollToTop />
+					<main>{children}</main>
+					<button onClick={changeTheme} class="fixed bottom-3 right-3 bg-white dark:bg-black border-2 border-gray-800 dark:border-gray-400 p-1 rounded-full">
+						<ThemeIcon className="w-6 h-6 text-gray-800 dark:text-gray-400" />
+					</button>
+					<Footer />
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
