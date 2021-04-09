c0bc55ecbd304d0416ffcdb2a7cc4b7e4cf249a3
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-09 04:19:14 +0200

    wip

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,26 @@
+import NProgress from 'nprogress'
 import 'tailwindcss/tailwind.css'
 import '@/styles/custom.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	NProgress.configure({ showSpinner: false })
+
+	useEffect(() => {
+		router.events.on('routeChangeStart', () => NProgress.start())
+		router.events.on('routeChangeComplete', () => NProgress.done())
+		router.events.on('routeChangeError', () => NProgress.done())
+
+		return () => {
+			router.events.off('routeChangeStart', () => NProgress.start())
+			router.events.off('routeChangeComplete', () => NProgress.done())
+			router.events.off('routeChangeError', () => NProgress.done())
+		}
+	})
+
 	return <Component {...pageProps} />
 }
 
